06f7593c5a01bde03d65d62294f16f34217bff72
eidankhan <eidan.khan659@gmail.com>
2024-05-24 21:39:01 +0100

    Generating KeyPairs

---
+++ src/main/java/com/banking/app/KeyPairGeneratorUtil.java
@@ -0,0 +1,26 @@
+package com.banking.app;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class KeyPairGeneratorUtil {
+
+    private final KeyPair keyPair;
+
+    public KeyPairGeneratorUtil() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        this.keyPair = keyGen.generateKeyPair();
+    }
+
+    public PrivateKey getPrivateKey() {
+        return keyPair.getPrivate();
+    }
+
+    public PublicKey getPublicKey() {
+        return keyPair.getPublic();
+    }
+}
